2d271fb83c60a2543d366206be705463500ad74f
KimDongYun <67079959+ehddbs3408@users.noreply.github.com>
2023-10-29 03:28:48 +0900

    File organize

--- HTML/dist/Gameobject.js
+++
@@ -1,9 +0,0 @@
-import { Rect } from "./Rect.js";
-export class GameObject {
-    constructor(x, y, width, height) {
-        this.rect = new Rect(x, y, width, height);
-    }
-    translate(delta) {
-        this.rect.pos.add(delta);
-    }
-}
--- HTML/nodeSample.js
+++
@@ -1,3 +0,0 @@
-console.log("HelloWorld");
-
-//typeScript & npm
